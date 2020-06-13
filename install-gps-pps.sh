94a035becb0ecd39be50a2ac7f876c13e52f6177
beta-tester <alpha-beta-release@gmx.net>
2020-06-13 13:28:28 +0200

    fixed ListenStream

--- install-gps-pps.sh
+++ install-gps-pps.sh
@@ -79,8 +79,8 @@ EOF
     grep -q mod_install_stratum_one /lib/systemd/system/gpsd.socket &>/dev/null || {
         echo -e "\e[36m    fix gpsd to listen to all connection requests\e[0m";
         tar -ravf $BACKUP_FILE -C / lib/systemd/system/gpsd.socket
-        sudo sed /lib/systemd/system/gpsd.socket -i -e "s/ListenStream=[::1]:2947/ListenStream=2947/";
-        sudo sed /lib/systemd/system/gpsd.socket -i -e "s/ListenStream=127.0.0.1:2947/#ListenStream=127.0.0.1:2947/";
+        sudo sed /lib/systemd/system/gpsd.socket -i -e "s/^ListenStream=\[::1\]:2947/ListenStream=2947/";
+        sudo sed /lib/systemd/system/gpsd.socket -i -e "s/^ListenStream=127.0.0.1:2947/#ListenStream=127.0.0.1:2947/";
         cat << EOF | sudo tee -a /lib/systemd/system/gpsd.socket &>/dev/null
 ;; mod_install_stratum_one
 EOF
